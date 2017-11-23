ac73a10bdc70f9358ad88df48b1ea0a019dd854b
Developer <developer@IM-M11-408.local>
2017-11-23 12:29:36 -0600

    added mongodb uri a

--- src/containers/about.js
+++ src/containers/about.js
@@ -15,13 +15,16 @@ export default class About extends Component {
           <Row>
 
             <Col xs="6" sm="4">
-              <h1>Underground Music Oriented Platform</h1>
+              <h3>Underground Music Oriented Platform</h3>
+                <p>have a band? want to be heard by everyone but none of the bureaucracy?</p>
             </Col>
             <Col xs="6" sm="4">
-              <h1>Music, not Business.</h1>
+              <h3>Music, not Business.</h3>
+              <p> we care about Music, we want to be the "must be" place to start your band career to success</p>
             </Col>
             <Col xs="6" sm="4">
-              <h1>Jumpstart the best underground bands from everywhere!.</h1>
+              <h3>Jumpstart the best underground bands from everywhere!.</h3>
+              <p>Want to hear what's on the minds of real musicians, who doesnt have a totally plastic producer behind? this is the place!</p>
             </Col>
           </Row>
         </Container>
