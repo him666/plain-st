e6449ee779021cbca89034a4dda1de9f4d788cf9
him666 <all.existance@gmail.com>
2018-04-02 19:49:24 -0500

    added about page

--- src/containers/about.js
+++ src/containers/about.js
@@ -1,24 +1,37 @@
 import React, {Component} from 'react'
 import {Container, Col, Row} from 'reactstrap';
-import  Header  from '../components/header'
+import  Header  from '../components/header';
+import musocial from '../assets/music-social-group.png';
+import munotes from '../assets/musical-notes.png';
+import mubuss from '../assets/handshake.png';
+
 export default class About extends Component {
   render() {
     return (
       <div>
         <Header />
-        <Container>  
+        <Container>
           <Row>
-            <Col xs="6" sm="4">
-              <h3>Underground Music Oriented Platform</h3>
-                <p>have a band? want to be heard by everyone but none of the bureaucracy?</p>
+            <Col xs="12" sm="4" md="4">
+              
+                <img src={mubuss} alt="mubuss" className="imageus"/>
+                  <h3>Underground Music Oriented Platform</h3>
+                    <p>have a band? want to be heard by everyone but none of the bureaucracy?</p>
+              
             </Col>
-            <Col xs="6" sm="4">
-              <h3>Music, not Business.</h3>
-              <p> we care about Music, we want to be the "must be" place to start your band career to success</p>
+            <Col xs="12" sm="4" md="4">
+              
+                <img src={munotes} alt="munotes" className="imageus"/>
+                <h3>Music, not Business.</h3>
+                <p> we care about Music, we want to be the "must be" place to start your band career to success</p>
+              
             </Col>
-            <Col xs="6" sm="4">
-              <h3>Jumpstart the best underground bands from everywhere!.</h3>
-              <p>Want to hear what's on the minds of real musicians, who doesnt have a totally plastic producer behind? this is the place!</p>
+            <Col xs="12" sm="4" md="4">
+              
+                <img src={musocial} alt="musocial" className="imageus"/>
+                <h3>Jumpstart the best underground bands from everywhere!.</h3>
+                <p>Want to hear what's on the minds of real musicians, who doesnt have a totally plastic producer behind? this is the place!</p>
+              
             </Col>
           </Row>
         </Container>
