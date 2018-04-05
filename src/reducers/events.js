55322518605f2cbb5cf10756cb223e2f3ac3ab63
Victor Huerta Hernández <vhuertahnz@gmail.com>
2018-04-05 11:29:47 -0500

    Fixing conflict

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -3,7 +3,6 @@ import { GET_EVENTS } from "../actions";
 const initialState = [];
 
 export function getEvents(state = initialState, action) {
-<<<<<<< HEAD
   switch (action.type) {
     case GET_EVENTS:
       console.log(action.payload);
@@ -12,15 +11,4 @@ export function getEvents(state = initialState, action) {
     default:
       return state;
   }
-=======
-    switch (action.type) {
-        case GET_EVENTS:
-            console.log(action.payload)
-            return Object.assign({}, state, { ...action.payload })
-        
-        default:
-            return (state)
-    }
-
->>>>>>> 6c4f213df7c5b5a7ff37d0f0b951cb31897a747f
 }
