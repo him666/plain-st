1949e96ad0899c6c1db5389272f8c4bc4fa4f0c8
Hector Marin <all.existance@gmail.com>
2018-04-05 11:52:15 -0500

    added news component

--- src/containers/news.js
+++ src/containers/news.js
@@ -27,21 +27,37 @@ class News extends Component {
         <Header />
         <Container>
           <Row>
-            <Col xs="12" sm="4" md="4" />
-            <Col xs="12" sm="4" md="4">
+            <Col xs="12" md="12">
+             <h1> Próximos Eventos: </h1>
+            </Col>
+            <Col xs="12" md="12">
               <div>
-                <img src={img1} alt="news" className="imageus" />
-                {this.props.events.length > 0 ? (
-                  <p> good </p>
-                ) : (
-                  <h1> No bands to Display </h1>
-                )}
+                <Container>
+                 {
+                   this.props.events.length > 0 ? 
+                      this.props.events.map((ev) => {
+                       return (
+                        <div>
+                          <Row key={ev.id}>
+                           <h1 className="title">{ev.attributes.title} </h1>
+                           <p className= "description"> Descripcion: {ev.attributes.content}</p>
+                           <img src={ev.attributes.header} className="banner"/>
+                          </Row>
+                        </div>
+                       )
+                      })
+                    : 
+                    <div>
+                      <img src={img1} alt="news" className="imageus"/>
+                      <h1> No news to Display </h1>
+                    </div>
+                 } </Container>
               </div>
             </Col>
           </Row>
         </Container>
       </div>
-    );
+    )
   }
 }
 function mapStateToProps(state) {
